7030ed92053f8c7ff2b9b0cb42018243c775e544
martins20 <martins20.tech@gmail.com>
2021-12-17 10:27:05 -0300

    feat: implement feature of useState Hook

--- src/pages/hooks/UseState/index.tsx
+++ src/pages/hooks/UseState/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-// import Counter from "../../../components/organisms/Counter";
+import Counter from "../../../components/organisms/Counter";
 import PageHeader from "../../../components/molecules/PageHeader";
 import SplitHookContent from "../../../components/templates/SplitHookContent";
 
@@ -8,12 +8,28 @@ import { Container } from "./styles";
 
 const UseState: React.FC = () => {
   // TODO: Use Counter with useState Hook.
+  const [counterNumber, SetCounterNumber] = useState(0);
 
   return (
     <Container>
       <PageHeader title="useState" />
 
-      <SplitHookContent topics={[]}>{/* <Counter /> */}</SplitHookContent>
+      <SplitHookContent
+        topics={[
+          "It's a state manager.",
+          "Return array of 2 types, an actual state and a callback to update state.",
+          "Can be started with a callback.",
+        ]}
+      >
+        <Counter
+          counter={counterNumber}
+          handleIncrement={() => SetCounterNumber(state => state + 1)}
+          handleDecrement={() =>
+            SetCounterNumber(state => (state > 0 ? state - 1 : state))
+          }
+          handleResetCounter={() => SetCounterNumber(0)}
+        />
+      </SplitHookContent>
     </Container>
   );
 };

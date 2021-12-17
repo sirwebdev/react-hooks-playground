28bdc187bf2881349cdbe544e31dbb9a48612220
martins20 <martins20.tech@gmail.com>
2021-12-17 11:40:19 -0300

    feat: add useMemo features

--- src/pages/hooks/UseMemo/index.tsx
+++ src/pages/hooks/UseMemo/index.tsx
@@ -1,4 +1,6 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useMemo } from "react";
+
+import useTopic from "../../../hooks/useTopic";
 
 import Counter from "../../../components/organisms/Counter";
 import PageHeader from "../../../components/molecules/PageHeader";
@@ -8,6 +10,7 @@ import { Container } from "./styles";
 
 const UseMemo: React.FC = () => {
   const [counterNumber, setCounterNumber] = useState(0);
+  const { selectedTopics } = useTopic();
 
   const handleIncrement = useCallback(
     () => setCounterNumber(state => state + 1),
@@ -21,20 +24,27 @@ const UseMemo: React.FC = () => {
 
   const handleResetCounter = useCallback(() => setCounterNumber(0), []);
 
-  // TODO: Make a pure component.
   // TODO: Get the counterNumber by useMemo.
+  const counter = useMemo((): number => counterNumber, [counterNumber]);
+
+  // TODO: Make a pure component.
+  const counterComponent = useMemo(() => {
+    return (
+      <Counter
+        counter={counter}
+        handleIncrement={handleIncrement}
+        handleDecrement={handleDecrement}
+        handleResetCounter={handleResetCounter}
+      />
+    );
+  }, [counter, handleIncrement, handleDecrement, handleResetCounter]);
 
   return (
     <Container>
       <PageHeader title="useMemo" />
 
-      <SplitHookContent topics={[]}>
-        <Counter
-          counter={counterNumber}
-          handleIncrement={handleIncrement}
-          handleDecrement={handleDecrement}
-          handleResetCounter={handleResetCounter}
-        />
+      <SplitHookContent topics={selectedTopics}>
+        {counterComponent}
       </SplitHookContent>
     </Container>
   );

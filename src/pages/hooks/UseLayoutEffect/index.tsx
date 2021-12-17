0598fe9eacf8b183c571f677d8e6ce4c7dfd753a
martins20 <martins20.tech@gmail.com>
2021-12-17 12:05:25 -0300

    feat: add useLayoutEffect features

--- src/pages/hooks/UseLayoutEffect/index.tsx
+++ src/pages/hooks/UseLayoutEffect/index.tsx
@@ -1,14 +1,23 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 
 import Counter from "../../../components/organisms/Counter";
 import PageHeader from "../../../components/molecules/PageHeader";
 import SplitHookContent from "../../../components/templates/SplitHookContent";
 
 import { Container } from "./styles";
+import useTopic from "../../../hooks/useTopic";
 
 const UseLayoutEffect: React.FC = () => {
-  const [counterNumber, setCounterNumber] = useState(0);
   const counterRef = useRef<HTMLDivElement>(null);
+  const [counterNumber, setCounterNumber] = useState(0);
+
+  const { selectedTopics } = useTopic();
 
   const handleIncrement = useCallback(
     () => setCounterNumber(state => state + 1),
@@ -23,12 +32,37 @@ const UseLayoutEffect: React.FC = () => {
   const handleResetCounter = useCallback(() => setCounterNumber(0), []);
 
   // TODO: Show the difference of synchronization between useEffect and useLayoutEffect.
+  // useLayoutEffect(() => {
+  //   if (!counterRef.current) return;
+
+  //   const { bottom } = counterRef.current.getBoundingClientRect();
+
+  //   counterRef.current.style.bottom = `${bottom - 300}px`;
+  // }, [counterRef]);
+
+  // useEffect(() => {
+  //   console.log("Calling the useEffect callback");
+
+  //   if (!counterRef.current) return;
+
+  //   const { bottom } = counterRef.current.getBoundingClientRect();
+
+  //   counterRef.current.style.bottom = `${bottom - 300}px`;
+  // }, [counterRef]);
+
+  useLayoutEffect(() => {
+    console.log("Calling the useLayoutEffect callback");
+  }, []);
+
+  useEffect(() => {
+    console.log("Calling the useEffect callback");
+  }, []);
 
   return (
     <Container>
       <PageHeader title="useLayoutEffect" />
 
-      <SplitHookContent topics={[]}>
+      <SplitHookContent topics={selectedTopics}>
         <section ref={counterRef} style={{ position: "absolute" }}>
           <Counter
             counter={counterNumber}

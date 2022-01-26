586f0a542605f487a94976fda0c46542a4c6dea7
martins20 <martins20.tech@gmail.com>
2022-01-26 16:07:22 -0300

    test: ensure home screen redirect to hooks page when click into button

--- src/pages/Home/index.spec.tsx
+++ src/pages/Home/index.spec.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { render, cleanup, RenderResult } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  RenderResult,
+  fireEvent,
+} from "@testing-library/react";
 
 import Sut from ".";
 
 let sut: RenderResult;
 
+const mockNavigate = jest.fn();
+
 jest.mock("react-router-dom", () => ({
-  useNavigate: jest.fn(),
+  useNavigate: () => (to: string) => mockNavigate(to),
 }));
 
 describe("Home", () => {
@@ -24,5 +31,18 @@ describe("Home", () => {
     const HomeElement = getByTestId("home_container");
 
     expect(HomeElement).toBeTruthy();
+  });
+
+  it("Should be able to navigate to hooks screen when clicks 'Start Discover' button", () => {
+    const { getByText } = sut;
+
+    const ButtonElement = getByText("Start discover");
+    expect(ButtonElement).toBeTruthy();
+
+    fireEvent.click(ButtonElement);
+
+    expect(mockNavigate).toHaveBeenCalled();
+    expect(mockNavigate).toHaveBeenCalledTimes(1);
+    expect(mockNavigate).toHaveBeenCalledWith("hooks");
   });
 });
